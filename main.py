3c489a6624e3f111294d66916882f0b04390bec9
SiBaiBaShi <conichi@foxmail.com>
2017-09-15 20:43:57 +0800

    增加下载MySQL信息于excel选项；调整参数位置，功能放在前方

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 
 import download
 import enviroment
+import excel
 import favor
 import index
 
@@ -14,36 +15,45 @@ if __name__ == '__main__':
                                                  u'默认保存于C:\\Users\\root\Pictures\same\download\\'
                                      .encode('GBK'),
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-s', nargs='?', const='all', default=False,
-                        help=u'show,显示预设频道信息；无参数则显示全部，格式：“频道名”'
+    parser.add_argument('-dc', nargs='*', default=[],
+                        help=u'download channel,频道名；'
                         .encode('GBK'))
-
-    parser.add_argument('-u', nargs='?', const='all', default=False,
-                        help=u'update,更新索引；无参数则更新全部，格式：“频道名”'.encode('GBK'))
-
-    parser.add_argument('-n', nargs='?', const=False, default=True,
-                        help=u'not,若不下载图片，输入此提示符即可'.encode('GBK'))
-    parser.add_argument('-c', nargs='*', default=[],
-                        help=u'channel,频道名；可多个参数，格式：“频道名”'.encode('GBK'))
-    parser.add_argument('-w', nargs='?', default='',
-                        help=u'where，sql过滤条件；如：where id=1 and views>100'.encode('GBK'))
-    parser.add_argument('-p', nargs='*', default=[],
+    parser.add_argument('-dr', nargs='?', default='',
+                        help=u'requirement，sql过滤条件；与sql条件语句完全一致'
+                        .encode('GBK'))
+    parser.add_argument('-dp', nargs='*', default=[],
                         help=u'path,保存路径；绝对路径，需有双引号'
                         .encode('GBK'))
 
-    parser.add_argument('-f', nargs='?', default=False,
-                        help=u'favor，更改favor值；格式：“（favor图片保存绝对路径）”'
+    parser.add_argument('-es', nargs='?', default='',
+                        help=u'excel sql，sql查询语句；与数据库环境完全一致'
+                        .encode('GBK'))
+    parser.add_argument('-ep', nargs='?', default='',
+                        help=u'path，excel保存路径；绝对路径，需加上excel文件名和尾缀'
+                        .encode('GBK'))
+
+    parser.add_argument('-ud', nargs='?', const='all', default=False,
+                        help=u'update database,更新数据库；无参数则更新所有频道'
+                        .encode('GBK'))
+    parser.add_argument('-uf', nargs='?', default=False,
+                        help=u'favor，更改favor值；文件夹绝对路径，需有双引号'
+                        .encode('GBK'))
+
+    parser.add_argument('-sd', nargs='?', const='all', default=False,
+                        help=u'show default,显示预设频道信息；无参数则显示所有频道'
                         .encode('GBK'))
     args = parser.parse_args()
 
-    if args.s:
-        enviroment.show(args.s)
+    if args.dc:
+        download.download_by_channel(args.dc, args.dr, args.dp)
 
-    if args.u:
-        index.update(args.u)
+    if args.es:
+        excel.get_data(args.es, args.ep)
 
-    if args.n:
-        download.download_by_channel(args.c, args.w, args.p)
+    if args.ud:
+        index.update(args.ud)
+    if args.uf:
+        favor.get_favor_list(args.uf)
 
-    if args.f:
-        favor.get_favor_list(args.f)
+    if args.sd:
+        enviroment.show(args.sd)
