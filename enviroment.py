2da201e051b4ec09c9d9953b230a00cc46e81d2c
SiBaiBaShi <conichi@foxmail.com>
2017-09-19 08:16:49 +0800

    修改部分bug，增加频道，可以录入更多信息

--- enviroment.py
+++ enviroment.py
@@ -4,6 +4,9 @@
 1.增加用户图片默认保存路径USER_PATH
 2.默认更新频道删除“轻性感”
 3.增加“这里只有帅哥美女”频道信息
+2017.9.18
+1.增加“晨间少女赏味期”和“短发控”频道信息
+2.operate_sql函数增加出错控制
 """
 import random
 import re
@@ -14,8 +17,17 @@ import requests
 
 PATH = r'C:\Users\root\Pictures\same\download\\'
 USER_PATH = r'C:\Users\root\Pictures\same\user\\'
-CHANNEL = [u'我这么美我不能死', u'长腿A杯', u'你觉得好看的samers', u'秀出你的身材',
-           u'S.T.S.B.H.Q', u'这里都是帅哥美女', u'DALUK', u'femininity']
+CHANNEL = [u'我这么美我不能死',
+           u'长腿A杯',
+           u'短发控',
+           u'你觉得好看的samers',
+           u'秀出你的身材',
+           u'S.T.S.B.H.Q',
+           u'晨间少女赏味期',
+           u'这里只有帅哥美女',
+           u'DALUK',
+           u'femininity']
+# u'足控只是会欣赏美',
 DB_INFO = {
     'host': "59.110.136.121",
     'user': "root",
@@ -24,25 +36,31 @@ DB_INFO = {
     'charset': "utf8"
 }
 ID = {
+    u'足控只是会欣赏美': 1011855,
     u'我这么美我不能死': 1015326,
     u'长腿A杯': 1032823,
     u'轻性感': 1033563,
+    u'短发控': 1057301,
     u'你觉得好看的samers': 1097342,
     u'秀出你的身材': 1112266,
     u'S.T.S.B.H.Q': 1125933,
-    u'这里都是帅哥美女': 1151333,
+    u'晨间少女赏味期': 1129604,
+    u'这里只有帅哥美女': 1151333,
     u'DALUK': 1166214,
     u'femininity': 1388511
 }
 URL = {
+    u'足控只是会欣赏美': "https://v2.same.com/channel/1011855/senses",
     u'我这么美我不能死': "https://v2.same.com/channel/1015326/senses",
     u'长腿A杯': "https://v2.same.com/channel/1032823/senses",
     u'轻性感': "https://v2.same.com/channel/1033563/senses",
+    u'短发控': "https://v2.same.com/channel/1057301/senses",
     u'你觉得好看的samers': "https://v2.same.com/channel/1097342/senses",
     u'秀出你的身材': "https://v2.same.com/channel/1112266/senses",
-    u'S.T.S.B.H.Q': "https://v2.same.com/channel/1125933/senses",
-    u'这里都是帅哥美女': "https://v2.same.com/channel/1151333/senses",
-    u'DALUK': "https://v2.same.com/channel/1166214/senses",
+    u'S.T.S.B.H.Q': "https://v2.same.com/channel/1125933/senses?offset=53271752",
+    u'晨间少女赏味期': "https://v2.same.com/channel/1129604/senses",
+    u'这里只有帅哥美女': "https://v2.same.com/channel/1151333/senses?offset=28224622",
+    u'DALUK': "https://v2.same.com/channel/1166214/senses?offset=65280361",
     u'femininity': "https://v2.same.com/channel/1388511/senses"
 }
 
@@ -95,12 +113,15 @@ def get_same_info(url):
 
 
 def operate_sql(db, sql):
-    cursor = db.cursor()
-    cursor.execute(sql)
-    db.commit()
-    temp = cursor.fetchall()
-    cursor.close()
-    return temp
+    try:
+        cursor = db.cursor()
+        cursor.execute(sql)
+        db.commit()
+        temp = cursor.fetchall()
+        cursor.close()
+        return temp
+    except:
+        print sql.decode('utf-8').encode('gbk', 'ignore')
 
 
 def show(channel_name):
